f14eb7f482bc9bb7b70cbdcc5f74322518dcd8a3
pedrofds87 <pedrofds@gmail.com>
2026-08-25 13:09:58 -0400

    Add macos/140_Installing_winget_into_machine.ps1

---
+++ macos/140_Installing_winget_into_machine.ps1
@@ -0,0 +1,14 @@
+# Script: Installing winget into machine
+# Platform: Windows
+# Description: #pstanczyk 2024-12-03
+# NinjaOne Script ID: 140
+
+$progressPreference = 'silentlyContinue'
+Write-Host "Installing WinGet PowerShell module from PSGallery..."
+Install-PackageProvider -Name NuGet -Force | Out-Null
+Install-Module -Name Microsoft.WinGet.Client -Force -Repository PSGallery | Out-Null
+Write-Host "Using Repair-WinGetPackageManager cmdlet to bootstrap WinGet..."
+Repair-WinGetPackageManager
+Write-Host "Done."
+
+#pstanczyk 2024-12-03
