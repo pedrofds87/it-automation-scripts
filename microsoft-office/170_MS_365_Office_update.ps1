e367d929d7739164468e6f5deafa82151db477a8
pedrofds87 <pedrofds@gmail.com>
2026-08-25 13:10:10 -0400

    Add microsoft-office/170_MS_365_Office_update.ps1

---
+++ microsoft-office/170_MS_365_Office_update.ps1
@@ -0,0 +1,16 @@
+# Script: MS 365 Office update
+# Platform: Windows
+# Description: # Force Office to update to the latest version on the Current Channel #pstanczyk
+# NinjaOne Script ID: 170
+
+# Force Office to update to the latest version on the Current Channel
+$OfficeUpdatePath = "${env:ProgramFiles}\Common Files\Microsoft Shared\ClickToRun\OfficeC2RClient.exe"
+
+if (Test-Path $OfficeUpdatePath) {
+    Start-Process -FilePath $OfficeUpdatePath -ArgumentList "/update user updatetoversion=Latest" -Wait
+    Write-Host "Office update initiated successfully."
+} else {
+    Write-Host "Office Click-to-Run update tool not found. Please check the Office installation."
+}
+
+#pstanczyk
