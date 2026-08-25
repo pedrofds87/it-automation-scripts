aa0259d1df3d158a45a0f8965a620c4706bbbf20
pedrofds87 <pedrofds@gmail.com>
2026-08-25 13:10:18 -0400

    Add microsoft-teams/107_Remove_Support_user_MS_Teams.ps1

---
+++ microsoft-teams/107_Remove_Support_user_MS_Teams.ps1
@@ -0,0 +1,8 @@
+# Script: Remove Support user MS Teams
+# Platform: Windows
+# Description: #made by pstanczyk 2024-11-06
+# NinjaOne Script ID: 107
+
+Remove-Item -Path "C:\Users\Support\AppData\Local\Microsoft\Teams\current" -Recurse -Force
+
+#made by pstanczyk 2024-11-06
