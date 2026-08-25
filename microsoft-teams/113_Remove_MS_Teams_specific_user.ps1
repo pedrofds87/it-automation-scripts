9e6881e31c00c0626d06ff3292ab0c96e8b15ab7
pedrofds87 <pedrofds@gmail.com>
2026-08-25 13:10:22 -0400

    Add microsoft-teams/113_Remove_MS_Teams_specific_user.ps1

---
+++ microsoft-teams/113_Remove_MS_Teams_specific_user.ps1
@@ -0,0 +1,24 @@
+# Script: Remove MS Teams specific user
+# Platform: Windows
+# Description: It needs the user as a parameter #made by pstanczyk 2024-11-06
+# NinjaOne Script ID: 113
+
+
+[string]$targetUsername = $env:tst
+
+
+# Define the user-specific Teams path based on the provided targetUsername parameter
+$teamsPath = "C:\Users\$targetUsername\AppData\Local\Microsoft\Teams"
+
+# Check if Teams is installed in the user-specific path and uninstall it if found
+if (Test-Path $teamsPath) {
+    Write-Output "Uninstalling Microsoft Teams for user $targetUsername..."
+    Stop-Process -Name Teams -Force -ErrorAction SilentlyContinue
+    Remove-Item -Recurse -Force -Path $teamsPath
+    Write-Output "Microsoft Teams has been uninstalled for user $targetUsername."
+} else {
+    Write-Output "Microsoft Teams is not found in the specified user path for user $targetUsername."
+}
+
+
+#made by pstanczyk 2024-11-06
