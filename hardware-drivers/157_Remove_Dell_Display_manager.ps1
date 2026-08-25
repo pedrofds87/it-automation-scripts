4b4f27cc32d771d71b6759e376aedf45a3756a95
pedrofds87 <pedrofds@gmail.com>
2026-08-25 13:09:51 -0400

    Add hardware-drivers/157_Remove_Dell_Display_manager.ps1

---
+++ hardware-drivers/157_Remove_Dell_Display_manager.ps1
@@ -0,0 +1,20 @@
+# Script: Remove Dell Display manager
+# Platform: Windows
+# Description: #pstanczyk
+# NinjaOne Script ID: 157
+
+# PowerShell script to run the Dell Display Manager uninstaller with no user interaction
+
+# Specify the path to the uninstaller
+$uninstallerPath = "C:\Program Files\Dell\Dell Display Manager 2\uninst.exe"
+
+# Check if the uninstaller exists
+if (Test-Path $uninstallerPath) {
+    # Execute the uninstaller with the /VERYSILENT parameter
+    Start-Process $uninstallerPath -ArgumentList "/VERYSILENT" -Wait
+    Write-Output "Dell Display Manager has been successfully uninstalled."
+} else {
+    # Output a message if the uninstaller does not exist
+    Write-Output "Uninstaller not found. Please check the path and try again."
+}
+#pstanczyk
