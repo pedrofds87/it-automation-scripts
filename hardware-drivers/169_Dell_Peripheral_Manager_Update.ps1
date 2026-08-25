11f68a68a24700e1426cf49c3f3721f519b4f4a7
pedrofds87 <pedrofds@gmail.com>
2026-08-25 13:09:52 -0400

    Add hardware-drivers/169_Dell_Peripheral_Manager_Update.ps1

---
+++ hardware-drivers/169_Dell_Peripheral_Manager_Update.ps1
@@ -0,0 +1,10 @@
+# Script: Dell Peripheral Manager Update
+# Platform: Windows
+# Description: #pstanczyk
+# NinjaOne Script ID: 169
+
+Start-Process -FilePath "C:\Temp\DPeM_7MGJH_1.7.7_WN64_A00.exe" -ArgumentList "/silent /norestart" -Wait
+
+
+
+#pstanczyk
