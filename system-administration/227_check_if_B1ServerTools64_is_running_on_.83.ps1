3697a05f0fb7b8ae90dd00b1faa8a22f1ad85076
pedrofds87 <pedrofds@gmail.com>
2026-08-25 13:11:29 -0400

    Add system-administration/227_check_if_B1ServerTools64_is_running_on_.83.ps1

---
+++ system-administration/227_check_if_B1ServerTools64_is_running_on_.83.ps1
@@ -0,0 +1,15 @@
+# Script: check if B1ServerTools64 is running on .83
+# Platform: Windows
+# Description: #pstanczyk
+# NinjaOne Script ID: 227
+
+$service = Get-Service -Name "B1ServerTools64" -ErrorAction SilentlyContinue
+
+if ($null -eq $service) {
+    Write-Output "Service B1ServerTools64 not found"
+}
+else {
+    Write-Output "Service: $($service.Name)"
+    Write-Output "Status : $($service.Status)"
+    Write-Output "Startup: $($service.StartType)"
+}
