f478edd210afae9c144728b9cba857e5dc283f32
pedrofds87 <pedrofds@gmail.com>
2026-08-25 13:11:00 -0400

    Add software-management/194_Company_Portal_(needs_winget,_runs_as_logged_user).ps1

---
+++ software-management/194_Company_Portal_(needs_winget,_runs_as_logged_user).ps1
@@ -0,0 +1,6 @@
+# Script: Company Portal (needs winget, runs as logged user)
+# Platform: Windows
+# Description: #pstanczyk
+# NinjaOne Script ID: 194
+
+winget install --id=Microsoft.CompanyPortal -e
