c68c0903840e44e3676594b96bfa7b6ea4af8bf6
pedrofds87 <pedrofds@gmail.com>
2026-08-25 13:11:01 -0400

    Add software-management/74_Slack_using_winget_Windows.ps1

---
+++ software-management/74_Slack_using_winget_Windows.ps1
@@ -0,0 +1,10 @@
+# Script: Slack using winget Windows
+# Platform: Windows
+# Description: #pstanczyk
+# NinjaOne Script ID: 74
+
+# Install or upgrade Slack
+winget install --id=SlackTechnologies.Slack -e --accept-source-agreements
+
+# Check the installed version
+winget list --id=SlackTechnologies.Slack
